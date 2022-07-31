a8e793a5281f66a3d75914801aad4df10592f893
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-07-30 17:18:52 -0700

    added condtional rendering for contact page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import ContactForm from './components/Contact';
 
 function App() {
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -28,12 +30,22 @@ function App() {
       categories={categories}
       setCurrentCategory={setCurrentCategory}
       currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
+          {/* <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        <About></About> */}
+        {!contactSelected ? (
+          <>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </div>
       </main>
     </div>
