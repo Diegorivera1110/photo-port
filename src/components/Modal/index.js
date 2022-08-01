270af764ab3fa18ae26d70a5ba5960bf6d09a95c
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-08-01 16:22:51 -0700

    created Modal component and formatted other components

---
+++ src/components/Modal/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Modal() {
+  return (
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">Photo Name</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,63 +1,70 @@
 import React, { useEffect } from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-
   const {
     categories = [],
-    setCurrentCategory, 
+    setCurrentCategory,
     currentCategory,
     contactSelected,
-    setContactSelected
+    setContactSelected,
   } = props;
-  
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
   }, [currentCategory]);
 
-    // function categorySelected(name) {
-    //     console.log(`${name} clicked`)
-    // }
+  // function categorySelected(name) {
+  //     console.log(`${name} clicked`)
+  // }
 
-    return (
-        <header className="flex-row px-1">
-  <h2>
-    <a data-testid="link" href="/">
-      <span role="img" aria-label="camera">
-        📸</span>
-         Oh Snap!
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className={`mx-2 ${contactSelected && 'navAcitve'}`}>
-        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-          About me
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera">
+            📸
+          </span>
+          Oh Snap!
         </a>
-      </li>
-      <li>
-        <span onClick={() => setContactSelected(true)}>Contact</span>
-      </li>
-      {categories.map((category) => (
-        <li
-        className={`mx-1 ${
-          currentCategory.name === category.name && !contactSelected && 'navActive'
-        }`} key={category.name}>
-            <span 
-            onClick={() => {
-              setCurrentCategory(category);
-              setContactSelected(false);
-              }} 
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className={`mx-2 ${contactSelected && "navAcitve"}`}>
+            <a
+              data-testid="about"
+              href="#about"
+              onClick={() => setContactSelected(false)}
+            >
+              About me
+            </a>
+          </li>
+          <li>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                "navActive"
+              }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
               >
                 {capitalizeFirstLetter(category.name)}
-            </span>
-        </li>
-      ))}
-    </ul>
-  </nav>
-</header>
-    );
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
